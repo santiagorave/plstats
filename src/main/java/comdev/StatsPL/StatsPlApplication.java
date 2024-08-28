6db257988db9a18596ddee4044fd33f2bbe82b6d
Xavi <santiagorave98@gmail.com>
2024-08-28 17:04:58 -0500

    Project Setup Spring

---
+++ src/main/java/comdev/StatsPL/StatsPlApplication.java
@@ -0,0 +1,13 @@
+package comdev.StatsPL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsPlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsPlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comdev/StatsPL/StatsPlApplicationTests.java
@@ -0,0 +1,13 @@
+package comdev.StatsPL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsPlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
